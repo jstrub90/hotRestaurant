31d0482d210a9a3825be2eaad8912c4d466578f5
J.Strub <jstrub90@gmail.com>
2019-02-23 10:03:35 -0500

    basic server set up and working

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // Dependencies
 // =============================================================
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 // Routes
@@ -18,4 +19,18 @@ app.use(express.json());
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
-  
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
